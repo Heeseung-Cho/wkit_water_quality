2734f0077b4efaca9f232df38ee70035ebf8f1a4
Heeseung-Cho <hscho9384@gmail.com>
2023-03-03 16:18:33 +0900

    [Modify]PEP8 Fix

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import argparse
 
 import wandb
 import torch
-from torch.utils.data import DataLoader
 import numpy as np
+import pandas as pd
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
-from utils.train import fit, test
 from models import DLinear
-from data_provider.data_factory import data_provider
+from utils.train import fit, test
+from utils.metrics import metric
+from data_provider.data_factory_wq import data_provider
+
 
 def set_seed(seed: int = 42):
     random.seed(seed)
@@ -20,81 +23,122 @@ def set_seed(seed: int = 42):
 
 
 def main(args):
-    use_wandb = args.use_wandb    
-    project_wandb = "WKIT"
-    run_name = f"DLinear_{args.data_path}_{args.target}_{args.seq_len}"    
-
-    save_path = f'{args.checkpoints}/{args.data_path}'
-    if not os.path.exists(save_path):
-        os.mkdir(save_path)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Setting Parameters
+    DATA_PATH = args.data_path
+    SAVE_PATH = f'{args.checkpoints}/{args.data_path}/{args.target}'
+    if not os.path.exists(SAVE_PATH):
+        os.mkdir(SAVE_PATH)
+    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     if torch.cuda.is_available():
-        print(f'CUDA is available. Your device is {device}.')
+        print(f'CUDA is available. Your device is {DEVICE}.')
     else:
-        print(f'CUDA is not available. Your device is {device}. It can take long time training in CPU.')    
-
-    ## Dataloader
+        print(f'CUDA is not available. Your device is {DEVICE}.')
+    # Model Parameters
+    TARGET = args.target
+    SEQ = args.seq_len
+    # Wandb Parameters
+    USE_WANDB = args.use_wandb
+    PROJECT_WANDB = "WKIT"
+    RUN_NAME = f"DLinear_{DATA_PATH}_{TARGET}_{SEQ}"
+    # Dataloader
     train_ds, train_loader = data_provider(args, flag='train')
-    _, vali_loader = data_provider(args, flag='val')    
-    test_ds, test_loader = data_provider(args, flag='test')    
-
+    _, vali_loader = data_provider(args, flag='val')
+    test_ds, test_loader = data_provider(args, flag='test')
     args.enc_in = train_ds.x_data.shape[1]
-    modelPath = os.path.join(save_path, f"DLinear_{args.target}_{args.seq_len}.pth")
+    MODEL_PATH = os.path.join(SAVE_PATH, f"DLinear_{TARGET}_{SEQ}.pth")
 
-    ## Model
+    # Model
     model = DLinear.Model(args)
-    if use_wandb:
-        wandb.init(project = project_wandb, config = args, name = run_name)
+    if USE_WANDB:
+        wandb.init(project=PROJECT_WANDB, config=args, name=RUN_NAME)
         wandb.watch(model, log="all")
     # train/validate now
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(params = model.parameters(),lr = args.learning_rate, weight_decay= args.learning_rate)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode = 'min', factor = 0.5, patience = 3)                
-    history = fit(epochs = args.train_epochs,
-                    model = model,
-                    train_loader = train_loader,
-                    val_loader = vali_loader,
-                    criterion = criterion, 
-                    optimizer = optimizer, 
-                    path = modelPath, 
-                    scheduler= scheduler,
-                    earlystop = 20,                      
-                    device = device,
-                    use_wandb = use_wandb)    
-    with open(os.path.join(save_path, f"hist_{args.target}_{args.seq_len}.pkl"), "wb") as file:
-        pickle.dump(history, file)    
-    if use_wandb:
+    optimizer = torch.optim.Adam(params=model.parameters(),
+                                 lr=args.learning_rate,
+                                 weight_decay=args.learning_rate
+                                 )
+    scheduler = CosineAnnealingLR(optimizer, T_max=10, eta_min=0)
+    history = fit(epochs=args.train_epochs,
+                  model=model,
+                  train_loader=train_loader,
+                  val_loader=vali_loader,
+                  criterion=criterion,
+                  optimizer=optimizer,
+                  path=MODEL_PATH,
+                  scheduler=scheduler,
+                  earlystop=30,
+                  device=DEVICE,
+                  use_wandb=USE_WANDB
+                  )
+    with open(os.path.join(SAVE_PATH, f"hist_{TARGET}_{SEQ}.pkl"), "wb") as f:
+        pickle.dump(history, f)
+    if USE_WANDB:
         wandb.finish()
-    pred = test(model, test_loader, device=device)
-    
-    import pandas as pd
-    df = pd.DataFrame([pred, test_ds.y_data], columns = range(10), index = [run_name+"_pred",run_name+"_real"])
-    df.to_csv(os.path.join(save_path,args.target+"_prediction.csv"))
-    
-if __name__ == "__main__":    
-    ## Argparse
-    parser = argparse.ArgumentParser(description='WKIT', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Metrics
+    metrics_df = dict()
+    ypred, ytrue = test(model, test_loader, device=USE_WANDB)
+    mae, mse, rmse, mape, _ = metric(ypred, ytrue)
+    print(f'MSE:{mse:.3f}, MAE:{mae:.3f}, MAPE:{mape:.3f}')
+    metrics_df["metrics_scale"] = np.array([mae, mse, rmse, mape])
+    if args.is_scale:
+        ypred = test_ds.inverse_transform(ypred.reshape(-1, 1)).flatten()
+        ytrue = test_ds.inverse_transform(ytrue.reshape(-1, 1)).flatten()
+        mae, mse, rmse, mape, _ = metric(ypred, ytrue)
+        metrics_df["metrics_real"] = np.array([mae, mse, rmse, mape])
+    metrics_df = pd.DataFrame.from_dict(metrics_df,
+                                        orient='index',
+                                        columns=["mae", "mse", "rmse", "mape"]
+                                        )
+    metrics_df.to_csv(os.path.join(SAVE_PATH,
+                                   TARGET+f"test_metrics_{SEQ}_report.csv")
+                      )
+
+    df = pd.DataFrame([ypred, ytrue],
+                      columns=range(10),
+                      index=[RUN_NAME+"_pred", RUN_NAME+"_real"])
+    df.to_csv(os.path.join(SAVE_PATH, TARGET+f"_test_{SEQ}_report.csv"))
+
+
+if __name__ == "__main__":
+    # Argparse
+    parser = argparse.ArgumentParser(description='WKIT')
 
     # data loader
-    parser.add_argument('--root_path', type=str, default="dataset/Water_Week", help='root path of the data file')
-    parser.add_argument('--data_path', type=str, default="D001청주정", help='data file')    
-    parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
-    parser.add_argument('--target', type=str, default='TOC', help='target feature in S or MS task')
-
+    parser.add_argument('--root_path', type=str, default="dataset/Water_Week",
+                        help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default="D001청주정",
+                        help='data file')
+    parser.add_argument('--checkpoints', type=str, default='./checkpoints/',
+                        help='location of model checkpoints')
+    parser.add_argument('--target', type=str, default='blue-green_algae',
+                        help='target feature in S or MS task')
+    parser.add_argument('--is_scale', type=int, default=1,
+                        help='Scaling to standardization, default True')
     # forecasting task
-    parser.add_argument('--seq_len', type=int, default=120, help='input sequence length')
-    parser.add_argument('--label_len', type=int, default=1, help='start token length')
-    parser.add_argument('--pred_len', type=int, default=1, help='prediction sequence length')
-
-   # optimization
-    parser.add_argument('--num_workers', type=int, default=4, help='data loader num workers')    
-    parser.add_argument('--train_epochs', type=int, default=3, help='train epochs')
-    parser.add_argument('--batch_size', type=int, default=8, help='batch size of train input data')
-    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
-    parser.add_argument('--learning_rate', type=float, default=1e-4, help='optimizer learning rate')
-    parser.add_argument('--loss', type=str, default='mse', help='loss function')
-
-    parser.add_argument('--use_wandb', type=int, default=0, help='(Option) logging on wandb')
+    parser.add_argument('--seq_len', type=int, default=120,
+                        help='input sequence length')
+    parser.add_argument('--label_len', type=int, default=1,
+                        help='start token length')
+    parser.add_argument('--pred_len', type=int, default=1,
+                        help='prediction sequence length')
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=4,
+                        help='data loader num workers')
+    parser.add_argument('--train_epochs', type=int, default=2,
+                        help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=8,
+                        help='batch size of train input data')
+    parser.add_argument('--patience', type=int, default=3,
+                        help='early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=1e-4,
+                        help='optimizer learning rate')
+    parser.add_argument('--loss', type=str, default='mse',
+                        help='loss function')
+    # Wandb Option
+    parser.add_argument('--use_wandb', type=int, default=0,
+                        help='(Option) logging on wandb')
     args = parser.parse_args()
     set_seed(42)
     main(args)
--- utils/train.py
+++ utils/train.py
@@ -1,116 +1,131 @@
-import torch
 import time
+
+import torch
+import wandb
 import numpy as np
 from tqdm import tqdm
-import wandb
 
-def fit(epochs, model, train_loader, val_loader, criterion, optimizer, path, scheduler = None, earlystop = 20, device="cuda", use_wandb = True):
-    ## Initial
-    torch.cuda.empty_cache()    
+
+def fit(epochs,
+        model,
+        train_loader,
+        val_loader,
+        criterion,
+        optimizer,
+        path,
+        scheduler=None,
+        earlystop=20,
+        device="cuda",
+        use_wandb=True):
+    # Initial
+    torch.cuda.empty_cache()
     train_losses = []
     val_losses = []
     min_loss = np.inf
-    not_improve = 0
-    warmup = 0
+    i = 0
+    warmup = 30
     model.to(device)
     if use_wandb:
         wandb.watch(model, log="all")
     fit_time = time.time()
 
-    ## Run epochs
+    # Run epochs
     for e in range(epochs):
         since = time.time()
-        running_loss = 0       
-        
+        running_loss = 0
         # training loop
         model.train()
         for _, data in enumerate(tqdm(train_loader)):
-            # training phase            
+            # training phase
             inputs, labels = data
             inputs = inputs.to(device).float()
             labels = labels.to(device).float()
-            optimizer.zero_grad()  # reset gradient
-            
+            # reset gradient
+            optimizer.zero_grad()
             # forward
-            
             outputs = model(inputs)
-            outputs = outputs[:,:,-1].flatten()
-            loss = criterion(outputs, labels)            
+            outputs = outputs[:, :, -1].flatten()
+            loss = criterion(outputs, labels)
 
             # backward
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-    
+
         model.eval()
         val_loss = 0
         # validation loop
         with torch.no_grad():
-            for _, data in enumerate(tqdm(val_loader)):                
+            for _, data in enumerate(tqdm(val_loader)):
                 inputs, labels = data
                 inputs = inputs.to(device).float()
                 labels = labels.to(device).float()
 
                 # forward
                 outputs = model(inputs)
-                outputs = outputs[:,:,-1].flatten()
+                outputs = outputs[:, :, -1].flatten()
                 # evaluation metrics
                 # loss
                 loss = criterion(outputs, labels)
-                val_loss += loss.item()                
+                val_loss += loss.item()
 
         # calculate mean for each batch
-        train_losses.append(running_loss / len(train_loader))
-        val_losses.append(val_loss / len(val_loader))
+        t_loss = running_loss / len(train_loader)
+        v_loss = val_loss / len(val_loader)
+        train_losses.append(t_loss)
+        val_losses.append(v_loss)
 
-
-        ## Warmup and Earlystopping
+        # Warmup and Earlystopping
         if e + 1 > warmup:
-            if min_loss - (val_loss / len(val_loader)) > 0.0001 :
-                print('Loss Decreasing {:.3f} >> {:.3f} . Save model to {} '.format(min_loss, (val_loss / len(val_loader)), path))
-                min_loss = (val_loss / len(val_loader))            
-#                torch.save(model.state_dict(), path)
-                not_improve = 0
-                
+            scheduler.step(val_loss)
+            if min_loss - v_loss > 0.0001:
+                print(f'Update {min_loss:.3f} >> {v_loss:.3f}. Save to {path}')
+                min_loss = v_loss
+                torch.save(model.state_dict(), path)
+                i = 0
             else:
-                not_improve += 1
-                print(f'Loss Not Decrease for {not_improve} time')
-                if not_improve == earlystop:
-                    print(f'Loss not decrease for {not_improve} times, Stop Training')
+                i += 1
+                print(f'Loss Not Decrease for {i} time')
+                if i == earlystop:
+                    print(f'Loss not decrease for {i} times, Stop Training')
                     break
-            if scheduler != None:   
-                scheduler.step(val_loss)            
         else:
             print(f"Warmup until {warmup} epochs")
 
-        print("Epoch:{}/{}..".format(e + 1, epochs),
-                "Train Loss: {:.3f}..".format(running_loss / len(train_loader)),
-                "Val Loss: {:.3f}..".format(val_loss / len(val_loader)),
-                "Learning Rate:{}..".format(scheduler.optimizer.param_groups[0]['lr']),
-                "Time: {:.2f}m".format((time.time() - since) / 60))
+        print(
+            "Epoch:{}/{}..".format(e + 1, epochs),
+            "Train Loss: {:.3f}..".format(t_loss),
+            "Val Loss: {:.3f}..".format(v_loss),
+            "LR:{}".format(scheduler.optimizer.param_groups[0]['lr']),
+            "Time: {:.2f}m".format((time.time() - since) / 60)
+            )
         if use_wandb:
-            metrics_log = {"train_epoch": e + 1,
-                        "Train Loss": running_loss / len(train_loader),
-                        "Val Loss": val_loss / len(val_loader),
-                        "Learning Rate":scheduler.optimizer.param_groups[0]['lr'],
-                        "Time": (time.time() - since) / 60
-                        }
+            metrics_log = {
+                "train_epoch": e + 1,
+                "Train Loss": running_loss / len(train_loader),
+                "Val Loss": val_loss / len(val_loader),
+                "Learning Rate": scheduler.optimizer.param_groups[0]['lr'],
+                "Time": (time.time() - since) / 60
+                }
             wandb.log(metrics_log)
-
-
     history = {'train_loss': train_losses, 'val_loss': val_losses}
     print('Total time: {:.2f} m'.format((time.time() - fit_time) / 60))
     return history
 
-def test(model, test_loader, device="cuda"):
+
+def test(model,
+         test_loader,
+         device="cuda"):
     model.eval()
     model.to(device)
-    Pred = []
+    yPred = []
+    yTrue = []
     with torch.no_grad():
         for _, data in enumerate(tqdm(test_loader)):
-            inputs, _ = data                    
-            inputs = inputs.to(device).float()            
+            inputs, labels = data
+            inputs = inputs.to(device).float()
             outputs = model(inputs)
-            outputs = outputs[:,:,-1].flatten()
-            Pred.extend(outputs.detach().cpu().numpy())                    
-    return np.array(Pred)
+            outputs = outputs[:, :, -1].flatten()
+            yPred.extend(outputs.detach().cpu().numpy())
+            yTrue.extend(labels.flatten().detach().cpu().numpy())
+    return np.array(yPred), np.array(yTrue)
